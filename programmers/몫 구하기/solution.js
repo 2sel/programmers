38586ca057d89990d34449331d9befe83e060ee2
2sel <80334149+2sel@users.noreply.github.com>
2022-11-10 12:10:45 +0900

    uploaded at 2022. 11. 10. 오후 12:10:44

---
+++ programmers/몫 구하기/solution.js
@@ -0,0 +1,7 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120805
+
+function solution(num1, num2) {
+    var answer = 
+    Math.floor(num1 / num2);
+    return answer
+}
