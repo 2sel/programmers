fff7696445e098e421e2291ccf2edb58a729b522
2sel <80334149+2sel@users.noreply.github.com>
2022-11-13 23:08:10 +0900

    uploaded at 2022. 11. 13. 오후 11:08:11

---
+++ programmers/두 수의 나눗셈/solution.js
@@ -0,0 +1,6 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120806
+
+function solution(num1, num2) {
+    var answer = (num1 / num2)*1000;
+    return Math.floor(answer);
+}
