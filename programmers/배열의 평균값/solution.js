bdf3cf6269fbd15a1fcd4a8ff5923788849cbe12
2sel <80334149+2sel@users.noreply.github.com>
2022-11-14 10:34:35 +0900

    uploaded at 2022. 11. 14. 오전 10:34:35

---
+++ programmers/배열의 평균값/solution.js
@@ -0,0 +1,14 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120817
+
+function solution(numbers) {
+    let n = 0;
+    let sum = 0;
+    for(let i=0; i<numbers.length; i++){
+            sum += numbers[i]}
+        
+         n = sum / numbers.length
+         return n
+        }
+
+// 매개변수가 for문에 어떻게 작용하는지 알 필요가 있음
+   
