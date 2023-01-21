6d0459187e4b271167fc299a124bbf20c6c28547
2sel <80334149+2sel@users.noreply.github.com>
2023-01-21 23:41:14 +0900

    updated at 2023. 1. 21. 오후 11:41:14

--- programmers/짝수와 홀수/solution.js
+++ programmers/짝수와 홀수/solution.js
@@ -1,5 +1,5 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12937
 
 function solution(num) {
-    if (num % 2 ===0){return "Even"} else {return "Odd"}
+    return num % 2 ? "Odd" : "Even";
 }
