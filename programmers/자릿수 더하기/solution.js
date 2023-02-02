a5766e5c82fddc4896c0be7a24bd7d017e46308e
2sel <80334149+2sel@users.noreply.github.com>
2023-02-02 23:44:55 +0900

    updated at 2023. 2. 2. 오후 11:44:55

--- programmers/자릿수 더하기/solution.js
+++ programmers/자릿수 더하기/solution.js
@@ -1,5 +1,12 @@
-// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120906?language=javascript
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12931
 
-function solution(n){
-    return (n+"").split("").reduce((acc, curr) => acc + parseInt(curr), 0)
+function solution(n)
+{
+let answer = 0;
+    let m = String(n);
+    
+    for(let i = 0; i<m.length; i ++){
+        answer += parseInt(m[i]);
+    }
+    return answer;
 }
