5f1c6291e49e2640a457d37e74b15f080942681b
2sel <80334149+2sel@users.noreply.github.com>
2023-03-01 23:09:49 +0900

    uploaded at 2023. 3. 1. 오후 11:09:48

---
+++ programmers/인덱스 바꾸기/solution.js
@@ -0,0 +1,9 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120895
+
+function solution(my_string, num1, num2) {
+    const result = [...my_string]
+    result.splice(num1,1,my_string[num2])
+    result.splice(num2,1,my_string[num1])
+    return result.join("")
+
+}
