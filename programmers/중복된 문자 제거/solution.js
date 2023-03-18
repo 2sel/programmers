feff0f1faa368f704d9cb33f797315a2fd969f89
2sel <80334149+2sel@users.noreply.github.com>
2023-03-19 00:18:40 +0900

    updated at 2023. 3. 19. 오전 12:18:40

--- programmers/중복된 문자 제거/solution.js
+++ programmers/중복된 문자 제거/solution.js
@@ -1,7 +1,9 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120888
 
-function solution(my_string) {
-     const arr = my_string.split("");
-    const notArr = Array.from(new Set(arr));
-    return notArr.join("");
-}
+// function solution(my_string) {
+//      const arr = my_string.split("");
+//     const notArr = Array.from(new Set(arr));
+//     return notArr.join("");
+// }
+
+const solution = my_string => [...new Set(my_string)].join('')
