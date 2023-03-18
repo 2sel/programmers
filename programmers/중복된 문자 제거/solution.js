e44992696278658278915a061251fa00aed2417e
2sel <80334149+2sel@users.noreply.github.com>
2023-03-18 23:59:29 +0900

    uploaded at 2023. 3. 18. 오후 11:59:31

---
+++ programmers/중복된 문자 제거/solution.js
@@ -0,0 +1,7 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120888
+
+function solution(my_string) {
+     const arr = my_string.split("");
+    const notArr = Array.from(new Set(arr));
+    return notArr.join("");
+}
