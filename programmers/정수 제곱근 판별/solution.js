c2e3def45b30dc999e294bd9565f2e0f8eb6f28d
2sel <80334149+2sel@users.noreply.github.com>
2023-03-27 00:03:48 +0900

    updated at 2023. 3. 27. 오전 12:03:50

--- programmers/정수 제곱근 판별/solution.js
+++ programmers/정수 제곱근 판별/solution.js
@@ -1,11 +1,16 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12934
 
 function solution(n) {
-    if(Number.isInteger(Math.sqrt(n))){
-        return Math.pow(Math.sqrt(n)+1, 2)
+    var answer = 0;
+
+    for(let i=0, a=0; i<=n; i++){
+      if(n/i===i){
+        a = i + 1
+        return a*a
+      } else {
+        answer = -1
+      }
     }
-    else {
-        return -1
-    }
+
+    return answer;
 }
-// x의 제곱 리턴하는 조건이랑 x+1 제곱리턴하는거 너무 어렵다...;;;
