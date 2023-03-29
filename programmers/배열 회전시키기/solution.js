edb61375d06f83140a37434b1ca90461c7d0143d
2sel <80334149+2sel@users.noreply.github.com>
2023-03-29 23:45:24 +0900

    uploaded at 2023. 3. 29. 오후 11:45:23

---
+++ programmers/배열 회전시키기/solution.js
@@ -0,0 +1,10 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120844
+
+function solution(numbers, direction) {
+    if(direction === 'right'){
+        numbers.unshift(numbers.pop())
+    } else {
+        numbers.push(numbers.shift())
+    }
+    return numbers
+}
