57a5002106784160dd4e33da92476d37681ce50a
2sel <80334149+2sel@users.noreply.github.com>
2023-04-01 01:24:39 +0900

    updated at 2023. 4. 1. 오전 1:24:40

--- programmers/숫자 찾기/solution.js
+++ programmers/숫자 찾기/solution.js
@@ -1,4 +1,6 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120904
 
-const solution = (num,k)=>
+const solution = (num,k) =>
 [...num+''].indexOf(k+'') !== -1 ? [...num+''].indexOf(k+'')+1 : -1
+
+// [+ '']와 (k+'')는 split을 대신 해주는 개념으로 개꼼수다... 타입스크립트에서 진행할 경우 에러가 난다고 한다...
