acd3f1c2b70f5d1995a254582dca5dd6236d6bdf
2sel <80334149+2sel@users.noreply.github.com>
2023-04-05 22:51:21 +0900

    updated at 2023. 4. 5. 오후 10:51:20

--- programmers/합성수 찾기/solution.js
+++ programmers/합성수 찾기/solution.js
@@ -1,6 +1,7 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120846
 
 // 합성수는 소수가 아닌 자연수로 소수를 찾고 걸러내야 함
+// 다른 분 코드 참고
 function solution(n) {
     let arr = new Set();
     for(let i = 1; i <= n; i++){
