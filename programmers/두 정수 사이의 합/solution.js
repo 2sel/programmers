ed4c8f10b4feea9079f80b1fed862f9d36b5a6a9
2sel <80334149+2sel@users.noreply.github.com>
2023-04-13 22:41:00 +0900

    updated at 2023. 4. 13. 오후 10:41:00

--- programmers/두 정수 사이의 합/solution.js
+++ programmers/두 정수 사이의 합/solution.js
@@ -2,5 +2,7 @@
 
 function solution(a, b) {
     // 가우스 공식 = n(a+b)/2
+    // n을 a와 b만을 표현하기 위해 a,b만 이루어진 식은 ((b-a)+1)(a+b)/2 이다.
+    // 다만 a와 b의 대소관계가 정해져 있지 않기 때문에 절대값으로 Math.abs(a-b)를 둔다.
     return (Math.abs(a-b)+1)*(a+b)/2
 }
