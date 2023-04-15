7e6705e204f75e51ed8a6307da6137c942a963e6
2sel <80334149+2sel@users.noreply.github.com>
2023-04-15 23:53:07 +0900

    updated at 2023. 4. 15. 오후 11:53:08

--- programmers/A로 B 만들기/solution.js
+++ programmers/A로 B 만들기/solution.js
@@ -1,8 +1,5 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120886
 
 function solution(before, after) {
-    // 정렬하고 리턴
-    before = [...before].sort()
-    after = [...after].sort()
-    return before.filter((a,i) => a === after[i]).length === after.length ? 1 : 0
+    return before.split('').sort().join('') === after.split('').sort().join('') ? 1 : 0;
 }
