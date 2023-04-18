1f4f32fe200fa323389a9f97598e59cf805c5f2a
2sel <80334149+2sel@users.noreply.github.com>
2023-04-18 23:43:22 +0900

    updated at 2023. 4. 18. 오후 11:43:22

--- programmers/문자열을 정수로 바꾸기/solution.js
+++ programmers/문자열을 정수로 바꾸기/solution.js
@@ -2,5 +2,8 @@
 
 function solution(s) {
     return Number(s);
-    //return s/1 로도 풀이 가능
+    // return s/1 로도 풀이 가능
+    // return  +s;
+    // 문자열을 숫자 연산자와 함께 사용하면 자바스크립트가 계산을 위해 자동으로
+    // 문자열을 숫자로 바꾸어주기 때문에 풀이가 가능하다.
 }
