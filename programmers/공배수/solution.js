feec4dbe8cb4150cbcc49b0be6651b8c2f580d39
2sel <80334149+2sel@users.noreply.github.com>
2023-05-06 14:04:57 +0900

    uploaded at 2023. 5. 6. 오후 2:04:56

---
+++ programmers/공배수/solution.js
@@ -0,0 +1,5 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181936
+
+function solution(number, n, m) {
+    if(number % n === 0 && number % m === 0){return 1}else{return 0}
+}
