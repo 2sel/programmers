7ab3a0f2f68f6bf5e124eea61a17f569eefad7d6
2sel <80334149+2sel@users.noreply.github.com>
2023-05-08 23:59:56 +0900

    uploaded at 2023. 5. 8. 오후 11:59:57

---
+++ programmers/부분 문자열인지 확인하기/solution.js
@@ -0,0 +1,5 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181843
+
+function solution(my_string, target) {
+    if(my_string.includes(target)){return 1} else {return 0}
+}
