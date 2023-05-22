bcc8f4fc7e6e46712fad663934e6da3d63d1e4c6
2sel <80334149+2sel@users.noreply.github.com>
2023-05-22 23:22:24 +0900

    uploaded at 2023. 5. 22. 오후 11:22:24

---
+++ programmers/배열에서 문자열 대소문자 변환하기/solution.js
@@ -0,0 +1,16 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181875
+
+function solution(strArr) {
+    let answer = [];
+
+    for(let i = 0; i<strArr.length; i++){
+        if(i%2==0){
+            answer.push(strArr[i].toLowerCase())
+        }
+        else{
+            answer.push(strArr[i].toUpperCase())
+        }
+    }
+
+    return answer;
+}
