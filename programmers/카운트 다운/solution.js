bb617a60e86fd9307d69a9848371f34e573189a2
2sel <80334149+2sel@users.noreply.github.com>
2023-05-30 23:47:18 +0900

    updated at 2023. 5. 30. 오후 11:47:19

--- programmers/카운트 다운/solution.js
+++ programmers/카운트 다운/solution.js
@@ -1,5 +1,11 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181899
 
 function solution(start, end) {
-    return Array.from({length: start + 1}, (_, i) => i).splice(end).reverse();
+    var answer = [];
+
+    for(let i = start; i>=end; i--){
+        answer.push(i)
+    }
+
+    return answer;
 }
