30fc52e3832c390812d569830099bb2cf746a3bc
2sel <80334149+2sel@users.noreply.github.com>
2023-06-12 19:27:04 +0900

    uploaded at 2023. 6. 12. 오후 7:27:03

---
+++ programmers/홀짝 구분하기/solution.js
@@ -0,0 +1,22 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181944
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    n = Number(input[0]);
+    
+    let preparedString = 'odd'
+    if(n%2===0){
+        preparedString = 'even'
+    }
+    
+    console.log (`${n} is ${preparedString}`)
+});
