b050a66942395deeb00d6030ba79eaf13d8cfba7
2sel <80334149+2sel@users.noreply.github.com>
2023-06-18 23:36:05 +0900

    uploaded at 2023. 6. 18. 오후 11:36:06

---
+++ programmers/배열 비교하기/solution.js
@@ -0,0 +1,18 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181856
+
+function solution(arr1, arr2) {
+    const arr1Sum = arr1.reduce((acc, cur) => acc + cur, 0);
+    const arr2Sum = arr2.reduce((acc, cur) => acc + cur, 0);
+
+    if (arr1.length === arr2.length) {
+        if (arr1Sum > arr2Sum) {
+            return 1;
+        } else if (arr2Sum > arr1Sum) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    return arr1.length > arr2.length ? 1 : -1;
+}
