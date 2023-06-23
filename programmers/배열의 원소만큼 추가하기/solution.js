10061692679e92a3fb5aba852f9e93b1232fd24d
2sel <80334149+2sel@users.noreply.github.com>
2023-06-23 18:27:27 +0900

    updated at 2023. 6. 23. 오후 6:27:29

--- programmers/배열의 원소만큼 추가하기/solution.js
+++ programmers/배열의 원소만큼 추가하기/solution.js
@@ -1,11 +1,5 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181861
 
 function solution(arr) {
-      let answer = [];
-    for(let i=0;i<arr.length;i++){
-        for(let j=0;j<arr[i];j++){
-             answer.push(arr[i]);
-        }
-    }
-    return answer;
+    return arr.reduce((list, num) => [...list, ...new Array(num).fill(num)], []);
 }
