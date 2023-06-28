35a80087ecc91fa6422629ee5662d25dcb8c28a3
2sel <80334149+2sel@users.noreply.github.com>
2023-06-28 22:46:52 +0900

    updated at 2023. 6. 28. 오후 10:46:52

--- programmers/l로 만들기/solution.js
+++ programmers/l로 만들기/solution.js
@@ -5,3 +5,4 @@ function solution(myString) {
 }
 
 // charCodeAt() 메서드는 주어진 인덱스에 대한 UTF-16 코드를 나타내는 0부터 65535 사이의 정수를 반환. 알파벳은 총 108개로 이루어져 있기 때문에 사용.
+// myString.replace(/[a-k]/g,'l') << 정규 표현식 사용시 짧고 간편하게 기입가능
