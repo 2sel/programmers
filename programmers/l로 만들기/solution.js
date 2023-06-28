bb275f6d902e809dae0f4d7916c09931aa272959
2sel <80334149+2sel@users.noreply.github.com>
2023-06-28 22:46:06 +0900

    updated at 2023. 6. 28. 오후 10:46:06

--- programmers/l로 만들기/solution.js
+++ programmers/l로 만들기/solution.js
@@ -3,3 +3,5 @@
 function solution(myString) {
      return [...myString].map(a => a.charCodeAt() < 108 ? 'l' : a).join("")
 }
+
+// charCodeAt() 메서드는 주어진 인덱스에 대한 UTF-16 코드를 나타내는 0부터 65535 사이의 정수를 반환. 알파벳은 총 108개로 이루어져 있기 때문에 사용.
