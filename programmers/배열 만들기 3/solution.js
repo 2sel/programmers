48748fe7a6e07af40d9bdfdb36ddf20fdccc12f0
2sel <80334149+2sel@users.noreply.github.com>
2023-07-24 22:28:32 +0900

    updated at 2023. 7. 24. 오후 10:28:32

--- programmers/배열 만들기 3/solution.js
+++ programmers/배열 만들기 3/solution.js
@@ -4,3 +4,4 @@ function solution(arr, intervals) {
     let [[a,b],[c,d]] = intervals
     return [...arr.slice(a, b+1), ...arr.slice(c, d+1)]
 }
+// 구조분해할당 사용하여 intervals로 만든 후 식을 괄호로 감싸서(입출력 예시 참고) return. 구조분해할당은 서치참고
