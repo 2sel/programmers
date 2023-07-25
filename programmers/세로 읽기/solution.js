0295f96419a18604a42750975d65ac15050c28f5
2sel <80334149+2sel@users.noreply.github.com>
2023-07-25 23:42:15 +0900

    updated at 2023. 7. 25. 오후 11:42:15

--- programmers/세로 읽기/solution.js
+++ programmers/세로 읽기/solution.js
@@ -3,9 +3,10 @@
 function solution(my_string, m, c) {
     let result =''
   
-    for(i=c-1; i<my_string.length; i += m){
-    result += my_string[i]
+    for(let i = 0; i<my_string.length; i += m){
+    result += my_string[i+ c -1]
     }
     
     return result
 }
+// i의 범위 지정을 못해서 틀렸는데, 서치로 보안했다.
