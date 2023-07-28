9aa5dab83f3aa115ce0e012cee8e3a31ccfd61d0
2sel <80334149+2sel@users.noreply.github.com>
2023-07-28 23:36:11 +0900

    updated at 2023. 7. 28. 오후 11:36:12

--- programmers/홀수 vs 짝수/solution.js
+++ programmers/홀수 vs 짝수/solution.js
@@ -3,11 +3,16 @@
 function solution(num_list) {
    let even = 0;
    let odd = 0;
-   
+   // 1. map을 돌리면서 %으로 홀짝을 나눈다.
    num_list.map((a, b) => {
+       // 2. 짝수라면 even에 더한다.
+       // 3. 홀수라면 odd에 더한다.
        !(b%2) ? even += a : odd +=a
    }) 
     
+   // 4. 둘의 값을 비교하여 큰 값을 리턴하게 한다. 
    return odd > even ? odd : even
 }
+
+
 
