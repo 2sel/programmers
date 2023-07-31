e00b8b3b1796b61676294fa75fb01dcdb1d3027f
2sel <80334149+2sel@users.noreply.github.com>
2023-08-01 08:08:32 +0900

    updated at 2023. 8. 1. 오전 8:08:32

--- programmers/모스부호 (1)/solution.js
+++ programmers/모스부호 (1)/solution.js
@@ -32,4 +32,5 @@ function solution(letter) {
     
     return letter.split(' ').map(a => morse[a]).join('')
 }
-// 제한사항에서 언급 된 공백을 제거 후 result에 맞게 따옴표 추가
+// 제한사항에서 언급 된 공백을 제거 후 result에 맞게 따옴표 추가.
+// reduce((prev, curr) => prev + morse[curr], '') 로 map 대신 reduce 로 풀이 가능
