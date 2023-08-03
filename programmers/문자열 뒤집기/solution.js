0c68b17e67ee929538ce1644282602cd01b2140e
2sel <80334149+2sel@users.noreply.github.com>
2023-08-03 10:13:01 +0900

    updated at 2023. 8. 3. 오전 10:13:01

--- programmers/문자열 뒤집기/solution.js
+++ programmers/문자열 뒤집기/solution.js
@@ -1,5 +1,9 @@
-// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120822
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181905
 
-function solution(my_string) {
-    return my_string.split("").reverse().join("");
+function solution(my_string, s, e) {
+    let params = my_string.substring(s, e + 1).split("").reverse();
+    let arr = my_string.split("");
+    arr.splice(s, e - s + 1, ...params);
+
+  return arr.join("");
 }
