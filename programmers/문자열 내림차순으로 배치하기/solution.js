2ecb6d60b0222683ff58c26b6d3816c053fbacc7
2sel <80334149+2sel@users.noreply.github.com>
2023-08-06 19:08:44 +0900

    updated at 2023. 8. 6. 오후 7:08:45

--- programmers/문자열 내림차순으로 배치하기/solution.js
+++ programmers/문자열 내림차순으로 배치하기/solution.js
@@ -3,4 +3,5 @@
 function solution(s) {
     return s.split("").sort().reverse().join("")
 }
-// sort()메서드는 오름차순이기 때문에 정렬을 하고 reverse()를 사용하여 내림차순을 만든다
+// sort()메서드는 오름차순이기 때문에 정렬을 하고 reverse()를 사용하여 내림차순을 만든다.
+// 내림차순을 할거라면 sort((a,b) => a<b ? 1:-1) 사용
