425db0849e959813044be7dfbf30f65a3d97c5ec
2sel <80334149+2sel@users.noreply.github.com>
2023-08-07 20:39:39 +0900

    updated at 2023. 8. 7. 오후 8:39:38

--- programmers/콜라츠 수열 만들기/solution.js
+++ programmers/콜라츠 수열 만들기/solution.js
@@ -2,8 +2,8 @@
 
 function solution(n) {
     let answer = [];
-    
     while(n !== 1){
+        // answer.push(n) 이 첫 실행문인 이유는 결과 값에 '시작점인 n'도 포함이 되어야 하기 때문이라고 함!(서치)
         answer.push(n)
         if(n%2===0){
             n = n / 2
@@ -14,5 +14,4 @@ function solution(n) {
     answer.push(1)
     return answer
 }
-// 콜라츠 추측 수열 참고
-// https://www.folivoralab.com/254
+// 콜라츠 추측 수열 참고(링크) = https://www.folivoralab.com/254
