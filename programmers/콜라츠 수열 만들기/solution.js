d444ccd560b600cac0133cd74a55a43a4e81512e
2sel <80334149+2sel@users.noreply.github.com>
2023-08-07 20:40:01 +0900

    updated at 2023. 8. 7. 오후 8:40:00

--- programmers/콜라츠 수열 만들기/solution.js
+++ programmers/콜라츠 수열 만들기/solution.js
@@ -3,7 +3,7 @@
 function solution(n) {
     let answer = [];
     while(n !== 1){
-        // answer.push(n) 이 첫 실행문인 이유는 결과 값에 '시작점인 n'도 포함이 되어야 하기 때문이라고 함!(서치)
+    // answer.push(n) 이 첫 실행문인 이유는 결과 값에 '시작점인 n'도 포함이 되어야 하기 때문이라고 함!(서치)
         answer.push(n)
         if(n%2===0){
             n = n / 2
