919ffcf0355a99f934288f74a205f27a9c8cba76
2sel <80334149+2sel@users.noreply.github.com>
2023-08-17 23:18:06 +0900

    updated at 2023. 8. 17. 오후 11:18:06

--- programmers/직사각형 별찍기/solution.js
+++ programmers/직사각형 별찍기/solution.js
@@ -4,10 +4,10 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-    for(let i = 0; i<b; i++){
-        let str = "";
-        for(let j =0; j<a; j++){
-            str = str + "*"
+    for(let i = 0; i<b; i++){ // i 선언, b만큼 반복
+        let str = ""; // 변수 선언
+        for(let j =0; j<a; j++){ // j 선언, 별을 줄당 몇 개 찍을지 반복
+            str = str + "*" // 변수에 별 담기
         }
         console.log(str)
     }
