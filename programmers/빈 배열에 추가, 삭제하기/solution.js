0d2c6c7f57b74a7889dffa41bd27526aafe7a82b
2sel <80334149+2sel@users.noreply.github.com>
2023-08-24 23:14:34 +0900

    uploaded at 2023. 8. 24. 오후 11:14:33

---
+++ programmers/빈 배열에 추가, 삭제하기/solution.js
@@ -0,0 +1,15 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181860
+
+function solution(arr, flag) {
+    let answer = [];
+    
+    for(let i=0; i<flag.length; i++){
+        if(flag[i]){
+            answer.push(...Array(arr[i]*2).fill(arr[i]))
+        } else{
+            answer.splice(answer.length - arr[i],arr[i])
+        }
+    }
+    
+    return answer
+}
