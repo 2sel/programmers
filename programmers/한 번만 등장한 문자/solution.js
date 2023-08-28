3a4c91ca96eb7627e47603324de07c172423ae98
2sel <80334149+2sel@users.noreply.github.com>
2023-08-28 16:55:46 +0900

    uploaded at 2023. 8. 28. 오후 4:55:44

---
+++ programmers/한 번만 등장한 문자/solution.js
@@ -0,0 +1,10 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120896
+
+function solution(s) {
+    let res = [];
+    for (let c of s) if (s.indexOf(c) === s.lastIndexOf(c)) res.push(c);
+    return res.sort().join('');
+}
+
+// for...of문을 사용함과 동시에 indexOf와 lastIndexOf를 사용하여 앞과 뒤를 추적하여 해당 값이 하나만 있을 경우 바로 정렬할 수 있도록 했다.
+// 풀리지 않아서 서치로 해결...! 
