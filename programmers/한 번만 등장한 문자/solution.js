88ae4e92996d0ca07a0886f1a858d5b6587c3e01
2sel <80334149+2sel@users.noreply.github.com>
2023-08-28 16:56:36 +0900

    updated at 2023. 8. 28. 오후 4:56:34

--- programmers/한 번만 등장한 문자/solution.js
+++ programmers/한 번만 등장한 문자/solution.js
@@ -8,3 +8,5 @@ function solution(s) {
 
 // for...of문을 사용함과 동시에 indexOf와 lastIndexOf를 사용하여 앞과 뒤를 추적하여 해당 값이 하나만 있을 경우 바로 정렬할 수 있도록 했다.
 // 풀리지 않아서 서치로 해결...! 
+// https://beomy.tistory.com/20
+// https://oliviakim.tistory.com/81
