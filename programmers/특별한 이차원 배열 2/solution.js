50cbe33009cf6383793c768107de422883a0ef07
2sel <80334149+2sel@users.noreply.github.com>
2023-08-29 21:12:53 +0900

    updated at 2023. 8. 29. 오후 9:12:52

--- programmers/특별한 이차원 배열 2/solution.js
+++ programmers/특별한 이차원 배열 2/solution.js
@@ -1,14 +1,14 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181831
 
 function solution(arr) {
-    let flag = true
-    for(let i= 0 ; i < arr.length ; i ++) {
-        for(let j = 0 ; j < arr[i].length ; j ++) {
-            if(arr[i][j] !== arr[j][i]) {
-                flag = false
-            }
+     const answer = arr.length;
+
+    for(let i=0; i<answer; i++) {
+        for(let j=i+1; j<answer; j++) {
+            if(arr[i][j] !== arr[j][i]) return 0;
         }
     }
-    
-    return ~~flag
+
+    return 1;
+
 }
