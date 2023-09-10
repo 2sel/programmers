e117601d708adc8b671005f8e54e31e7c309fefa
2sel <80334149+2sel@users.noreply.github.com>
2023-09-10 23:06:08 +0900

    uploaded at 2023. 9. 10. 오후 11:06:07

---
+++ programmers/잘라서 배열로 저장하기/solution.js
@@ -0,0 +1,11 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120913
+
+function solution(my_str, n) {
+    var answer = [];
+    
+    for(let i=0; i<my_str.length; i+=n){
+        answer.push(my_str.substr(i,n))
+    }
+    
+    return answer
+}
