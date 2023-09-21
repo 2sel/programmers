6de315720137d124de95166fff17002f168280d4
2sel <80334149+2sel@users.noreply.github.com>
2023-09-21 22:56:46 +0900

    updated at 2023. 9. 21. 오후 10:56:40

--- programmers/3진법 뒤집기/solution.js
+++ programmers/3진법 뒤집기/solution.js
@@ -4,3 +4,7 @@ function solution(n) {
     let reverse_th = n.toString(3).split('').reverse().join('')
     return parseInt(reverse_th, 3)
 }
+
+// 1. 숫자를 문자열로 변환(toString)
+// 2. 문자열을 분할, reverse로 앞뒤로 뒤집기
+// 3. join으로 붙히고 parseInt로 정수 변환. 3진법이라서 3 추가로 입력 
