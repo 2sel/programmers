1e12b2f780457627c34d1f441ab19fa744fec96c
2sel <80334149+2sel@users.noreply.github.com>
2023-10-04 23:25:52 +0900

    updated at 2023. 10. 4. 오후 11:25:47

--- programmers/구슬을 나누는 경우의 수/solution.js
+++ programmers/구슬을 나누는 경우의 수/solution.js
@@ -1,6 +1,11 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120840
 
 function solution(balls, share) {
-    var answer = 0;
-    return answer;
+    let factorial = [BigInt(1)];
+    
+    for(let i = 1; i<= balls; i++){
+        factorial[i] = factorial[i-1] * BigInt(i);
+    }
+    
+    return factorial[balls] / (factorial[balls - share] * factorial[share])
 }
