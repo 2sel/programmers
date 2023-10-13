76152d3b1154f1e17cb2d8092736a090ff39961a
2sel <80334149+2sel@users.noreply.github.com>
2023-10-13 23:43:45 +0900

    uploaded at 2023. 10. 13. 오후 11:43:40

---
+++ programmers/시저 암호/solution.js
@@ -0,0 +1,22 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12926
+
+function solution(s, n) {
+    let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let lower = "abcdefghijklmnopqrstuvwxyz"
+    
+    let answer = "";
+    for(let i=0; i<s.length; i++){
+        let text = s[i];
+        
+        if(text == " "){
+            answer += " ";
+            continue;
+        }
+        let textArr = upper.includes(text) ? upper : lower;
+        let index = textArr.indexOf(text) +n;
+        
+        if(index >= textArr.length) index -= textArr.length;
+        answer += textArr[index];
+    }
+    return answer;
+}
